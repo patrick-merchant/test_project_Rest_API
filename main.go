5c23ea5033a1d3735c9edf0b7f5a7fd6da214577
Patrick Merchant <patrickmerchant9@gmail.com>
2022-07-26 14:45:10 +0100

    adds go files

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Mimics the structure of the json placeholder endpoint
+type Post struct {
+	UserId string `json:"UserId"`
+	Id     string `json:"Id"`
+	Title  string `json:"Title"`
+	Body   string `json:"Body"`
+}
+
+var Posts []Post
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	var myRouter = mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/posts", returnAllPosts)
+	myRouter.HandleFunc("/post", createNewPost).Methods("POST")
+	myRouter.HandleFunc("/post/{id}", deletePost).Methods("DELETE")
+	myRouter.HandleFunc("/post/{id}", updatePost).Methods("PUT")
+	myRouter.HandleFunc("/post/{id}", returnSinglePost)
+
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+// Retrieving all posts
+func returnAllPosts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllPosts")
+	json.NewEncoder(w).Encode(Posts)
+}
+
+// Retrieving a single post
+func returnSinglePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, post := range Posts {
+		if post.Id == key {
+			json.NewEncoder(w).Encode(post)
+		}
+	}
+}
+
+// CREATE
+func createNewPost(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var post Post
+	json.Unmarshal(reqBody, &post)
+	Posts = append(Posts, post)
+
+	json.NewEncoder(w).Encode(post)
+}
+
+// DELETE
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	for index, post := range Posts {
+		if post.Id == id {
+			Posts = append(Posts[:index], Posts[index+1:]...)
+		}
+	}
+}
+
+// UPDATE
+func updatePost(w http.ResponseWriter, r *http.Request) {
+
+	// Parses HTTP request body and stores json in post variable (like CREATE)
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var post Post
+	json.Unmarshal(reqBody, &post)
+	json.NewEncoder(w).Encode(post)
+
+	// Loops over posts in Posts array and updates the selected post
+	vars := mux.Vars(r)
+	id := vars["id"]
+	for index, value := range Posts {
+		if value.Id == id {
+			Posts[index] = post
+			fmt.Println(Posts)
+		}
+	}
+}
+
+func main() {
+
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Posts = []Post{
+		Post{UserId: "1", Id: "1", Title: "A Post", Body: "This is a post"},
+		Post{UserId: "2", Id: "2", Title: "Another Post", Body: "This is another post"},
+	}
+	handleRequests()
+}
